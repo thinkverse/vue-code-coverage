1f01ee62246edbfcfec1bf00813f660a78b4b04f
thinkverse <ctuvedsson@gmail.com>
2019-04-21 04:11:00 +0200

    update: added functionality to button and display button press count

--- src/components/ButtonCount.vue
+++ src/components/ButtonCount.vue
@@ -1,9 +1,23 @@
 <template>
-  <h3>Welcome to button count application</h3>
+  <div>
+    <h3>Welcome to button count application</h3>
+    <p>Button count is: {{ count }}</p>
+    <button @click="changeCount">click</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ButtonCount'
+  name: 'ButtonCount',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    changeCount() {
+      this.count += 1;
+    },
+  },
 };
 </script>
