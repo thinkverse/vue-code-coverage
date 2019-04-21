1529098c0c3c55c8868ff2c36ce164137b84e460
thinkverse <ctuvedsson@gmail.com>
2019-04-21 04:24:23 +0200

    chore: Completed code coverage for ButtonCount component

--- tests/unit/buttonCount.spec.js
+++ tests/unit/buttonCount.spec.js
@@ -7,4 +7,19 @@ describe('ButtonCount.vue', () => {
   it('Register is a component', () => {
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
+
+  it('count is initially zero', () => {
+    expect(wrapper.vm.count).toBe(0);
+  });
+
+  it('is the button present', () => {
+    expect(wrapper.contains('button')).toBe(true);
+  });
+
+  it('counter incremented or not', () => {
+    expect(wrapper.vm.count).toBe(0);
+    const button = wrapper.find('button');
+    button.trigger('click');
+    expect(wrapper.vm.count).toBe(1);
+  });
 });
