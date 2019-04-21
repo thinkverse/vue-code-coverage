3e62523c6b1d994aa021b22fe2d1d6bdacb6fbaf
thinkverse <ctuvedsson@gmail.com>
2019-04-21 05:24:15 +0200

    fix: Added src folder to trigger Travis CI

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ButtonCount />
+  </div>
+</template>
+
+<script>
+import ButtonCount from './components/ButtonCount.vue';
+
+export default {
+  name: 'app',
+  components: {
+    ButtonCount
+  }
+};
+</script>
---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Welcome to button count application</h3>
+    <p>Button count is: {{ count }}</p>
+    <button @click="changeCount">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonCount',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    changeCount() {
+      this.count += 1;
+    },
+  },
+};
+</script>
